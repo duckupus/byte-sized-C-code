58a8dfe60a010cf1739f24e8fe45e6dabef9e2ae
duckupus <75720972+duckupus@users.noreply.github.com>
2021-10-22 11:53:43 +0800

    fixed bug that stopped opponent from playing

--- tictactoe.c
+++ tictactoe.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
 char board[3][3]={
 	{'1', '2', '3'},
 	{'4', '5', '6'},
 	{'7', '8', '9'}
 };
+
 char init_board[3][3]={
 	{'1', '2', '3'},
 	{'4', '5', '6'},
 	{'7', '8', '9'}
 };
+
 void p_board()	{
 	printf("%c|%c|%c", board[0][0], board[0][1], board[0][2]);
 	printf("\n=====\n");
@@ -18,6 +22,7 @@ void p_board()	{
 	printf("\n=====\n");
 	printf("%c|%c|%c\n", board[2][0], board[2][1], board[2][2]);
 }
+
 int change_board(int one, int two, char change) {
 	//first checks to ensure that it isn't already used
 	if(board[one][two] != init_board[one][two])
@@ -25,6 +30,7 @@ int change_board(int one, int two, char change) {
 	board[one][two]=change;
 	return 0;
 }
+
 char check() {
 	int i, q, nc=0;
 	char ret;
@@ -60,25 +66,28 @@ char check() {
 			}
 		}
 	}
-	if(nc==0) {
+	if(nc==0)
 		return 'f';
-	}
 	return '0';
 }
+
 void bot(char bott) {
 	int num, i=0, same;
 	//tries to randomise output
-	num=(rand() % 10);
+	num=(rand() % 9 + 1);
+	num--;
 	while(num > 3) {
-		num=num - 3;
-		++i;
+		num -= 3;
+		i++;
 	}
 	same=change_board(i, num, bott);
 	if(same==1)
 		bot(bott);
+	return;
 }
+
 int main()	{
-	srand(getpid());
+	srand(time(0));
 	char player='0', opponent;
 	int ip=0, i, same;
 	char w_condition='0';
@@ -95,7 +104,6 @@ int main()	{
 		opponent='X';
 	while(w_condition == '0') {
 		i=0;
-		system("clear");
 		p_board();
 		printf("choose a number from 1-9\n");
 		scanf("%d", &ip);
@@ -103,27 +111,33 @@ int main()	{
 		if(ip < 10 && ip > 0) {
 			ip--;
 			while(ip > 3) {
-				ip=ip - 3;
+				ip -= 3;
 				++i;
 			}
 		}
 		else
 			continue;
 		same=change_board(i, ip, player);
+		w_condition=check();
+		if(w_condition != '0') 
+			break;
 		if(same==1)
 			continue;
 		bot(opponent);
 		w_condition=check();
 	}
 	if(w_condition==player) {
+		p_board();
 		printf("You win!\n");
 		return 0;
 	}else if(w_condition==opponent) {
+		p_board();
 		printf("GG! try harder next time!\n");
 		return 1000;
-	}else if(w_condition=='f') {	
-			printf("Game has ended with a draw\n");
-			return  0;
+	}else if(w_condition=='f') {
+		p_board();
+		printf("Game has ended with a draw\n");
+		return  0;
 	}
 	return 0;
 }
