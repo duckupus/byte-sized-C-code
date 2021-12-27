ba9c3a96587e7886fd345918c8c24f8c9440b60e
duckupus <75720972+duckupus@users.noreply.github.com>
2021-12-27 20:17:03 +0800

    accepts a wider range of positive values

--- factorisation.c
+++ factorisation.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+/*
+ * NOTE: This program is not complete, optimisations(e.g. making it factorise the number once)
+ * are still in progress
+ */
+
+typedef unsigned long long int ulli;
 
 
-int *pvar[2];
+ulli *pvar[2];
 
-void innitpvar(int size) {
-	pvar[0]=(int *)malloc(size * sizeof(int));
-	pvar[1]=(int *)malloc(size * sizeof(int));
+void innitpvar(ulli size) {
+	pvar[0]=malloc(size * sizeof(ulli));
+	pvar[1]=malloc(size * sizeof(ulli));
 	if(pvar[0] == NULL || pvar[1] == NULL) {
 		perror("malloc");
 		exit(EXIT_FAILURE);
@@ -15,23 +23,23 @@ void innitpvar(int size) {
 
 
 int main(int argc, char **argv) {
-	int val, i;
-	int p, size, current, tval;
-	int j;
+	ulli val, i, p, size, current, tval;
+	ulli j;
+	time_t t_start, t_end;
 	if(argc == 1) {
 		printf("Usage: %s <numbers to be factorised>\n", argv[0]);
 		return 0;
 	}
 	for(i = 1; i < argc; i++) {
-		val=atoi(argv[i]);
+		p=sscanf(argv[i], "%lld", &val);
+		if(val <= 0 || p != 1)
+			continue;
 		tval=val;
 		p=2;
 		current=0;
 		size=1;
-		/*skips any negative values*/
-		if(val <= 0)
-			continue;
-		printf("value to be factorised: %d\n", val);
+		printf("value to be factorised: %lld\n", val);
+		t_start=time(NULL);
 		printf("running tests... ");
 		for(j=2; j < tval; j++) {
 			if(0==(tval % j)){
@@ -39,7 +47,9 @@ int main(int argc, char **argv) {
 				tval=tval / j;
 			}
 		}
+		t_end=time(NULL);
 		printf("Done!\n");
+		printf("eta time to factorize %lld: %ld\n", val, (t_end - t_start));
 		innitpvar(size);
 		while(p <= val) {
 			if(0==(val % p)) {
@@ -58,7 +68,7 @@ int main(int argc, char **argv) {
 		}
 		printf("prime\t|\tamount\n");
 		for(int a=1; a<=size; a++)
-			printf("%d\t|\t%d\n", pvar[0][a], pvar[1][a]);
+			printf("prime:%lld\t|\tnumber:%lld\n", pvar[0][a], pvar[1][a]);
 		free(pvar[0]);
 		free(pvar[1]);
 	}
