7981eb862cfb5498542e9d0f184c7c8727085d55
duckupus <75720972+duckupus@users.noreply.github.com>
2022-01-16 21:35:49 +0800

    slight optimizations

--- factorisation.c
+++ factorisation.c
@@ -2,55 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*
- * NOTE: This program is not complete, optimisations(e.g. making it factorise the number once)
- * are still in progress
- */
-
 typedef unsigned long long int ulli;
+size_t relloc_size;
 
 
 ulli *pvar[2];
 
-void innitpvar(ulli size) {
-	pvar[0]=malloc(size * sizeof(ulli));
-	pvar[1]=malloc(size * sizeof(ulli));
+void innitpvar() {
+	relloc_size=sizeof(ulli);
+	pvar[0]=malloc(relloc_size);
+	pvar[1]=malloc(relloc_size);
 	if(pvar[0] == NULL || pvar[1] == NULL) {
 		perror("malloc");
 		exit(EXIT_FAILURE);
 	}
 }
 
-
 int main(int argc, char **argv) {
-	ulli val, i, p, size, current, tval;
-	ulli j;
-	time_t t_start, t_end;
+	ulli val, i, p, current;
 	if(argc == 1) {
 		printf("Usage: %s <numbers to be factorised>\n", argv[0]);
 		return 0;
 	}
 	for(i = 1; i < argc; i++) {
-		p=sscanf(argv[i], "%lld", &val);
+		p=sscanf(argv[i], "%llu", &val);
 		if(val <= 0 || p != 1)
 			continue;
-		tval=val;
 		p=2;
 		current=0;
-		size=1;
-		printf("value to be factorised: %lld\n", val);
-		t_start=time(NULL);
-		printf("running tests... ");
-		for(j=2; j < tval; j++) {
-			if(0==(tval % j)){
-				size++;
-				tval=tval / j;
-			}
-		}
-		t_end=time(NULL);
-		printf("Done!\n");
-		printf("eta time to factorize %lld: %ld\n", val, (t_end - t_start));
-		innitpvar(size);
+		printf("value to be factorised: %llu\n", val);
+		innitpvar();
 		while(p <= val) {
 			if(0==(val % p)) {
 				if(pvar[0][current] == p) {
@@ -58,6 +39,9 @@ int main(int argc, char **argv) {
 				}
 				else {
 					/* no value of that in arr yet*/
+					relloc_size += sizeof(ulli);
+					pvar[0] = realloc(pvar[0], relloc_size);
+					pvar[1] = realloc(pvar[1], relloc_size);
 					current++;
 					pvar[0][current] = p;
 					pvar[1][current] = 1;
@@ -66,9 +50,9 @@ int main(int argc, char **argv) {
 			} else
 				p++;
 		}
-		printf("prime\t|\tamount\n");
-		for(int a=1; a<=size; a++)
-			printf("%lld\t|\t%lld\n", pvar[0][a], pvar[1][a]);
+		printf("prime\t|\tpower\n");
+		for(int a=1; a<=current; a++)
+			printf("%llu\t|\t%llu\n", pvar[0][a], pvar[1][a]);
 		free(pvar[0]);
 		free(pvar[1]);
 	}
