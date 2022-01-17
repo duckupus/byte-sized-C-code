250f02dc427c03d1a80fe60e780590fd27a99173
duckupus <75720972+duckupus@users.noreply.github.com>
2022-01-17 14:04:41 +0800

    speed to find prime numbers doubled

--- prime_printer.c
+++ prime_printer.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <math.h>
+
+/*
+ * NOTE: when compiling(with gcc) on linux, it may need a -lm flag
+ */
+
 int main(){
-	long int i, q, prime;
+	unsigned long long int i, q, prime;
 	i=1;
 	while(1){
 		i++;
 		prime=1;
-		for(q=2; q <= i / 2; ++q)	{
+		for(q=2; q <= sqrt((double)i); ++q)	{
 			if(i % q == 0) {
 				prime=0;
 				break;
 			}
 		}
 		if(prime == 1)
-			printf("%ld\n", i);
+			printf("%llu\n", i);
 	}
 	return 0;
 }
